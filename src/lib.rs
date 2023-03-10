b5d4a3fb2bf079ac33692b48364125a5611909fc
Alessandro Rezzi <alessandrorezzi2000@gmail.com>
2023-03-10 22:58:30 +0100

    Note storing + nullifier storing + shield balance

--- src/lib.rs
+++ src/lib.rs
@@ -36,11 +36,11 @@ pub struct JSExtendedSpendingKeySerData {
     pub account_index: u32,
 }
 
-//#[derive(Serialize, Deserialize)] //TODO: find a smart way to serialize and deserialize a MerklePath<Node>
+#[derive(Serialize, Deserialize)] //TODO: find a smart way to serialize and deserialize a MerklePath<Node>
 pub struct JSTxSaplingData {
-    pub decrypted_notes: Vec<(Note, MerklePath<Node>)>,
-    pub nullifier: Vec<Nullifier>,
-    pub commitment_tree: String, 
+    pub decrypted_notes: Vec<(Note, String)>,
+    pub nullifiers: Vec<String>,
+    pub commitment_tree: String,
 }
 
 //Generate an extended spending key given a seed, coin type and account index
@@ -127,25 +127,48 @@ pub fn handle_transaction(
         MAIN_NETWORK.hrp_sapling_extended_spending_key()
     };
 
+    let enc_str: &str = "p-secret-spending-key-test"; // ONLY FOR TESTING
     let extsk = encoding::decode_extended_spending_key(&enc_str, &enc_extsk)
         .expect("Cannot decode the extended spending key");
 
     let key = UnifiedFullViewingKey::new(Some(extsk.to_diversifiable_full_viewing_key()), None)
         .expect("Failed to create key");
+
+    let (nullifiers, comp_note) =
+        handle_transaction_internal(&mut tree, &tx, &key, true).expect("Cannot decode tx");
+    let mut ser_comp_note: Vec<(Note, String)> = vec![];
+    let mut ser_nullifiers: Vec<String> = vec![];
+    for x in comp_note.iter() {
+        let mut buff = Cursor::new(Vec::new());
+        x.1.write(&mut buff)
+            .expect("Cannot write witness to buffer");
+        let newNote = x.0.clone();
+        ser_comp_note.push((newNote, hex::encode(&buff.into_inner())));
+    }
+
+    for nullif in nullifiers.iter() {
+        ser_nullifiers.push(hex::encode(nullif.0));
+    }
+
     let mut buff = Cursor::new(Vec::new());
     tree.write(&mut buff).expect("Cannot write tree to buffer");
-    //let comp_note = handle_transaction_internal(&mut tree, &tx, &key, true).unwrap();
-    return serde_wasm_bindgen::to_value(&hex::encode(&buff.into_inner()))
-        .expect("Cannot serialize commitment tree");
+
+    let res: JSTxSaplingData = JSTxSaplingData {
+        decrypted_notes: ser_comp_note,
+        nullifiers: ser_nullifiers,
+        commitment_tree: hex::encode(buff.into_inner()),
+    };
+    return serde_wasm_bindgen::to_value(&res).expect("Cannot serialize tx output");
 }
 
 //TODO: move this function to the js end?
+/*
 fn handle_block(
     tree: &mut CommitmentTree<Node>,
     block_data: &str,
     key: &UnifiedFullViewingKey,
     is_testnet: bool,
-) -> Result<Vec<(Note, MerklePath<Node>)>, Box<dyn Error>> {
+) -> Result<Vec<(Note, IncrementalWitness<Node>)>, Box<dyn Error>> {
     let block_json: serde_json::Value = serde_json::from_str(block_data.trim()).unwrap();
     let mut notes = vec![];
     for tx in block_json.get("tx").unwrap().as_array().unwrap() {
@@ -154,14 +177,14 @@ fn handle_block(
     }
     return Ok(notes);
 }
-
+*/
 //add a tx to a given commitment tree and the return a witness to each output TODO: add a witness to each input as well
-fn handle_transaction_internal(
+pub fn handle_transaction_internal(
     tree: &mut CommitmentTree<Node>,
     tx: &str,
     key: &UnifiedFullViewingKey,
     is_testnet: bool,
-) -> Result<Vec<(Note, MerklePath<Node>)>, Box<dyn Error>> {
+) -> Result<(Vec<Nullifier>, Vec<(Note, IncrementalWitness<Node>)>), Box<dyn Error>> {
     let tx = Transaction::read(
         Cursor::new(hex::decode(tx)?),
         pivx_primitives::consensus::BranchId::Sapling,
@@ -174,8 +197,12 @@ fn handle_transaction_internal(
         decrypt_transaction(&MAIN_NETWORK, BlockHeight::from_u32(320), &tx, &hash)
     };
     let mut witnesses = vec![];
-
+    let mut nullifiers: Vec<Nullifier> = vec![];
     if let Some(sapling) = tx.sapling_bundle() {
+        for x in sapling.shielded_spends() {
+            nullifiers.push(*x.nullifier());
+        }
+
         for (i, out) in sapling.shielded_outputs().iter().enumerate() {
             println!("note found!");
             tree.append(Node::from_cmu(out.cmu()))
@@ -183,15 +210,13 @@ fn handle_transaction_internal(
             for note in &decrypted_tx {
                 if note.index == i {
                     // Save witness
-                    let witness = IncrementalWitness::from_tree(tree)
-                        .path()
-                        .expect("Note not found??");
+                    let witness = IncrementalWitness::from_tree(tree);
                     witnesses.push((note.note.clone(), witness));
                 }
             }
         }
     }
-    Ok(witnesses)
+    Ok((nullifiers, witnesses))
 }
 
 #[wasm_bindgen]
@@ -221,7 +246,10 @@ mod test {
         let tx = "0300000001a7d31ea039ab2a9914be2a84b6e8966758da5f8d1a64ac6fb49d2763dccc38da000000006b483045022100bb67345313edea3c7462c463ea8e03ef3b14caccfbefff9877ef246138427b6a02200b74211e1f27be080561c3985980e6b0e2e833f0751ea68dfb1e465b994afefc0121025c6802ec58464d8e65d5f01be0b7ce6e8404e4a99f28ea3bfe47efe40df9108cffffffff01e8657096050000001976a914b4f73d5c66d999699a4c38ba9fe851d7141f1afa88ac0000000001003665c4ffffffff00010b39ab5d98de6f5e3f50f3f075f61fea263b4cdd6927a53ac92c196be72911237f5041af34fed06560b8620e16652edf6d297d14a9cff2145731de6643d4bf13e189dbc4b6c4b91fe421133a2f257e5b516efd9b080814251ec0169fabdac1ce4a14575d3a42a7ca852c1ef6f6e1f3daf60e9ae4b77ef4d9a589dcbc09e8437fc28e80d6a0c4f1627b3e34ee9dd5cd587d1d57bab30e0a2eba893a6b61d7e53f5b49b4cb67a807e5db203b76744025d8395c83be2eb71009f9b82e78e7b65d9740340106ee59b22cd3628f1f10c3712c2b4f86464b627b27910cd3e0a80c5387798db4f15f751b5886beb1ab1a8c298185ed6f5d3a074689ba6e327e8dc2bd3b41790ecbe0240f909b8735b8ac98a59855b448e9f37d31d5d25b71959264c145abd15f0606ab5844391819afd4017890696272abad451dab8654d76e41c389941f0fd134d7d6e3b971b15cc63ba9bea421383639bdbeaa970636d637a1c6167154f39ded089d0f07776c58e8e86c0dac8259d22644e9d8a89456e9ccf2f66ce8633a9055f1703669c6a7b009865347ef608cb4ba8f3158e05947580ec50c32f69c0079dff58b3b53367f43490d4bcaba946ef4c42b4d366c66184f84ec442499a056b6b60eeaee94151459ac0b61eb6debfa96554bbe8ec39d2c49ee6eca48ed8dc137f84584803e2372ec35e0f9f4252beef9170419e703183fa87e7d35c2403b41700bc9f5d69da6c01c870515694f5c48372cba6bacd6a79ca1cdb85f38841f7680d0dd6853b22fc95d6e307419271edb05f2f40733c31c6f827eca592658716c5c73a9dd00a7e387250beffaa78bd1f104e031e00f014f9a50935864e11ffd655ea4d4c6c3d80b681e7581a19b2668c00528110ee5322add9dacb35b519280812050061788884cad7cc409a9261e86485cc4f2d904bdf40b3c78208a395a2488eb938b8a198b51ac418fa79e5d1d7bd8f96fe0910fe61136d8fe302f144745a988d6de83e89cd8befef8a762103aa32a14d93e3ac41b44188ab385b65c1f21cf29f19a6d2af556385dd60a994ecd1ac909488f7abce29e26690651a389d4466a9e20b7f08bfbdf4f4aa3e1577dc7debf1951688db8c75347d01e836f7816df3c7a7aaa833cbd6309d179d5dfc34045e52984cf475890f04b2ffcdf123175cc07568d08d9b8525ad9eabad231e1549a19fdce0fbb30c1fe7ec59bf8ed8e642ec6571456bdba8ade4458cffb1d65fee35242d7409de14a21514416a68e9c2d5c21eb9ca5813e1d8162a48d650ed7696b7b14b4f3d3f5eb892cf32614f62dea794e7f68e6d3d3ae6edf22e811f85e1ac7fe2a8437bdf287aa4d5ff842173039074516304042370a4e2feeb901319665ffc9b005b37c2afbea22faca316ea4f6b5f365fe46f679581966dadd029d687d2b400201";
         let key = UnifiedFullViewingKey::new(Some(skey.to_diversifiable_full_viewing_key()), None)
             .expect("Failed to create key");
-        let comp_note = handle_transaction_internal(&mut tree, tx, &key, true).unwrap();
+        let (nullifiers, comp_note) =
+            handle_transaction_internal(&mut tree, tx, &key, true).unwrap();
+        //This was a t-s tx
+        assert_eq!(nullifiers.len(), 0);
         //Successfully decrypt exactly 1 note
         assert_eq!(comp_note.len(), 1);
         let note = &comp_note[0].0;
