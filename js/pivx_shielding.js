f776ecba84a7915bf50311773b03b8a7de6abcd5
Duddino <duddino@duddino.com>
2023-03-14 11:53:33 +0100

    fix typo

--- js/pivx_shielding.js
+++ js/pivx_shielding.js
@@ -74,7 +74,7 @@ export default class PIVXShielding {
      */
     this.commitmentTree = commitmentTree;
     /**
-     * Hex encoded unspent notes (UTXO equivalent of shield)
+     * Hex encoded unspent notes (UTXO equivalent in shield)
      * @type {String[]}
      * @private
      */
