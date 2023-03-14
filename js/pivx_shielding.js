f6b1c73193bc09813d4c8c5dcbe016adb417fe92
Duddino <duddino@duddino.com>
2023-03-14 16:22:21 +0100

    Add jsdoc

--- js/pivx_shielding.js
+++ js/pivx_shielding.js
@@ -149,7 +149,7 @@ export default class PIVXShielding {
 
   /**
    * Createes a transaction, sending `amount` satoshis to the address
-   * @param {{address: String, amount: Number}} target
+   * @param {{address: String, amount: Number, blockHeight: Number, useShieldInputs: bool}} target
    */
   async createTransaction({address, amount, blockHeight, useShieldInputs = true}) {
     const { txid, txhex, nullifiers } = await this.shieldMan.create_transaction({
