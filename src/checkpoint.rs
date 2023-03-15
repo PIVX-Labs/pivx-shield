b537ad089db5abd0c812f61bd46ed047dcfb65ce
Duddino <duddino@duddino.com>
2023-03-15 10:45:53 +0100

    Added tests and improved old ones

--- src/checkpoint.rs
+++ src/checkpoint.rs
@@ -63,7 +63,7 @@ pub fn get_checkpoint(block_height: i32, is_testnet: bool) -> (i32, &'static str
     used_checkpoints
         .iter()
         .rev()
-        .find(|x| x.0 < block_height)
+        .find(|x| x.0 <= block_height)
         .copied()
         .unwrap_or(used_checkpoints[0])
 }
@@ -74,23 +74,38 @@ mod test {
     use crate::checkpoint::get_checkpoint;
     use pivx_primitives::merkle_tree::CommitmentTree;
     use pivx_primitives::sapling::Node;
+    use std::error::Error;
     use std::io::Cursor;
     #[test]
-    fn check_testnet_checkpoints() {
-        assert_eq!(get_checkpoint(11257770, true).0, 1125777);
+    fn check_testnet_checkpoints() -> Result<(), Box<dyn Error>> {
+        // Blocks above last checkpoint should yield last checkpoint
+        assert_eq!(get_checkpoint(1123200 * 5, true).0, 1123200);
+        // Blocks equal to last checkpoint should yield last checkpoint
+        assert_eq!(get_checkpoint(1123200, true).0, 1123200);
+        // Blocks in between two adjacent checkpoints should yield the smaller of the two
+        assert_eq!(get_checkpoint((907200 + 950400) / 2, true).0, 907200);
+        // Block 0 should yield an empty commitment tree
+        let tree = Cursor::new(hex::decode(get_checkpoint(0, true).1)?);
+        let tree = CommitmentTree::<Node>::read(tree)?;
+        assert_eq!(tree, CommitmentTree::empty());
+        Ok(())
     }
     #[test]
-    fn check_mainnet_checkpoints() {
+    fn check_mainnet_checkpoints() -> Result<(), Box<dyn Error>> {
+        // Blocks below shield activation height should return first checkpoint
         assert_eq!(get_checkpoint(2700000 - 1, false).0, 2700000);
+        // Blocks equal to shield activation height should return first checkpoint
+        assert_eq!(get_checkpoint(2700000, false).0, 2700000);
+        // Blocks near shield activation height should return first checkpoint
         assert_eq!(get_checkpoint(2700001, false).0, 2700000);
         assert_eq!(get_checkpoint(3758400 + 1, false).0, 3758400);
+        // Blocks in between two adjacent checkpoints should yield the smaller of the two
         assert_eq!(get_checkpoint((3758400 + 3715200) / 2, false).0, 3715200);
-        let buff = Cursor::new(
-            hex::decode(get_checkpoint(2700000, false).1)
-                .expect("Cannot decode commitment tree from hexadecimal"),
-        );
-        let tree = CommitmentTree::<Node>::read(buff).expect("Cannot decode commitment tree!");
-        assert_eq!(tree, CommitmentTree::<Node>::empty());
+        // First checkpoint should return an empty commitment tree
+        let buff = Cursor::new(hex::decode(get_checkpoint(2700000, false).1)?);
+        let tree = CommitmentTree::<Node>::read(buff)?;
+        assert_eq!(tree, CommitmentTree::empty());
+        Ok(())
     }
 }
 
