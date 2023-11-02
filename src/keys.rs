1d6dfda35ddac03de72ef18b2e4718cab8c6fd96
Alessandro Rezzi <alessandrorezzi2000@gmail.com>
2023-11-02 13:43:27 +0100

    Add extended full viewing key decoder/encoder/generator

--- src/keys.rs
+++ src/keys.rs
@@ -4,6 +4,7 @@ use pivx_primitives::consensus::{Parameters, MAIN_NETWORK, TEST_NETWORK};
 use wasm_bindgen::prelude::*;
 
 use pivx_primitives::sapling::PaymentAddress;
+use pivx_primitives::zip32::sapling::ExtendedFullViewingKey;
 use pivx_primitives::zip32::sapling::ExtendedSpendingKey;
 use pivx_primitives::zip32::AccountId;
 use pivx_primitives::zip32::DiversifierIndex;
@@ -46,14 +47,18 @@ pub fn decode_generic_address(
         Ok(GenericAddress::Transparent(to_address))
     }
 }
-pub fn decode_extsk(enc_extsk: &str, is_testnet: bool) -> Result<ExtendedSpendingKey, Box<dyn Error>> {
+pub fn decode_extsk(
+    enc_extsk: &str,
+    is_testnet: bool,
+) -> Result<ExtendedSpendingKey, Box<dyn Error>> {
     let enc_str: &str = if is_testnet {
         TEST_NETWORK.hrp_sapling_extended_spending_key()
     } else {
         MAIN_NETWORK.hrp_sapling_extended_spending_key()
     };
 
-    Ok(encoding::decode_extended_spending_key(enc_str, enc_extsk).map_err(|_| "Cannot decde extsk")?)
+    Ok(encoding::decode_extended_spending_key(enc_str, enc_extsk)
+        .map_err(|_| "Cannot decode extsk ")?)
 }
 
 pub fn encode_extsk(extsk: &ExtendedSpendingKey, is_testnet: bool) -> String {
@@ -73,6 +78,34 @@ pub fn encode_payment_address(addr: &PaymentAddress, is_testnet: bool) -> String
         MAIN_NETWORK.hrp_sapling_payment_address()
     };
     encoding::encode_payment_address(enc_str, addr)
+}
+
+pub fn decode_extended_full_viewing_key(
+    enc_extfvk: &str,
+    is_testnet: bool,
+) -> Result<ExtendedFullViewingKey, Box<dyn Error>> {
+    let enc_str: &str = if is_testnet {
+        TEST_NETWORK.hrp_sapling_extended_full_viewing_key()
+    } else {
+        MAIN_NETWORK.hrp_sapling_extended_full_viewing_key()
+    };
+
+    Ok(
+        encoding::decode_extended_full_viewing_key(enc_str, enc_extfvk)
+            .map_err(|_| "Cannot decode efvk")?,
+    )
+}
+
+pub fn encode_extended_full_viewing_key(
+    extfvk: &ExtendedFullViewingKey,
+    is_testnet: bool,
+) -> String {
+    let enc_str: &str = if is_testnet {
+        TEST_NETWORK.hrp_sapling_extended_full_viewing_key()
+    } else {
+        MAIN_NETWORK.hrp_sapling_extended_full_viewing_key()
+    };
+    encoding::encode_extended_full_viewing_key(enc_str, extfvk)
 }
 
 //Generate an extended spending key given a seed, coin type and account index
@@ -95,7 +128,10 @@ struct NewAddress {
 }
 //Generate the deafult address of a given encoded extended full viewing key
 #[wasm_bindgen]
-pub fn generate_default_payment_address(enc_extsk: String, is_testnet: bool) -> Result<JsValue, JsValue> {
+pub fn generate_default_payment_address(
+    enc_extsk: String,
+    is_testnet: bool,
+) -> Result<JsValue, JsValue> {
     let extsk = decode_extsk(&enc_extsk, is_testnet).map_err(|e| e.to_string())?;
     let (def_index, def_address) = extsk.to_diversifiable_full_viewing_key().default_address();
     Ok(serde_wasm_bindgen::to_value(&NewAddress {
@@ -128,4 +164,16 @@ pub fn generate_next_shielding_payment_address(
         address: encode_payment_address(&address, is_testnet),
         diversifier_index: new_index.0.to_vec(),
     })?)
+}
+
+// Generate the full viewing key given the extended spending key
+#[wasm_bindgen]
+pub fn generate_extended_full_viewing_key(
+    enc_extsk: String,
+    is_testnet: bool,
+) -> Result<JsValue, JsValue> {
+    let extsk = decode_extsk(&enc_extsk, is_testnet).map_err(|e| e.to_string())?;
+    let extfvk = extsk.to_extended_full_viewing_key();
+    let enc_extfvk = encode_extended_full_viewing_key(&extfvk, is_testnet);
+    Ok(serde_wasm_bindgen::to_value(&enc_extfvk)?)
 }
